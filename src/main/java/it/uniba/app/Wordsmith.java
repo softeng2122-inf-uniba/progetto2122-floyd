c02c70add291788df708b6e3780635b917e87d94
Nicolò <n.pino@studenti.uniba.it>
2022-05-14 15:26:27 +0200

    Effettuato override showHelp

--- src/main/java/it/uniba/app/Wordsmith.java
+++ src/main/java/it/uniba/app/Wordsmith.java
@@ -4,5 +4,9 @@ public class Wordsmith extends Player{
     public Wordsmith() {
         isWordsmith = true;
     }
+
+    public static void showHelp(){
+        System.out.println("\n/help\n/gioca\n/abbandona (disponibile solo durante la partita)\n/esci\n/nuova <parola> (disponibile solo durante la partita)\n/mostra (disponibile solo durante la partita)\n\n");
+    }
     
 }
