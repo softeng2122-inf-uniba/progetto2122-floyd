1e6c3702cbff91615ab69bd2d39b2db87bfd8333
Cry4775 <63257144+Cry4775@users.noreply.github.com>
2022-05-14 20:11:08 +0200

    Revert "Aggiunto help con elenco dei comandi"

--- src/test/java/it/uniba/app/AppTest.java
+++ src/test/java/it/uniba/app/AppTest.java
@@ -8,6 +8,13 @@ import org.junit.jupiter.api.Test;
  * Main test class of the application.
  */
 public class AppTest {
-  
-   
+    /**
+     * Test the getGreeting method of the App class.
+     */
+    @Test
+    public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(
+                "app should have a greeting", classUnderTest.getGreeting());
+    }
 }
