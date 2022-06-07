ba53548ff38d6fa5b41c793b73f0df0b22219751
Pierdamiano Zagaria <crysis3775@gmail.com>
2022-06-07 15:41:12 +0200

    Fixato errore di spotbugs

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -3,7 +3,6 @@ package it.uniba.app;
 import it.uniba.app.ui.UserInterface;
 import it.uniba.app.ui.control.HelpRequestProcessor;
 import it.uniba.app.user.UserController;
-import it.uniba.app.user.Wordsmith;
 import it.uniba.app.utils.UserInput;
 
 /**
@@ -17,7 +16,7 @@ public final class App {
      * @param args command line arguments
      */
     public static void main(final String[] args) {
-        UserController userController = new UserController(new Wordsmith());
+        UserController userController = new UserController("Wordsmith");
         UserInterface ui = new UserInterface(userController);
 
         UserInterface.printer.getWelcome();
--- src/main/java/it/uniba/app/user/UserController.java
+++ src/main/java/it/uniba/app/user/UserController.java
@@ -3,8 +3,15 @@ package it.uniba.app.user;
 public class UserController {
     private final Player user;
 
-    public UserController(final Player player) {
-        this.user = player;
+    public UserController(final String playerType) {
+        if (playerType.equals("Player")) {
+            this.user = new Player();
+        } else if (playerType.equals("Wordsmith")) {
+            this.user = new Wordsmith();
+        } else {
+            throw new IllegalArgumentException("Undefined player type." +
+                    "Must be Player or Wordsmith");
+        }
     }
 
     public boolean isWordsmith() {
