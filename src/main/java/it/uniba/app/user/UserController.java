61bdbab89c212ed313e6514d68aa512aa47c86e6
orsinico <nicoorsi.no@gmail.com>
2022-06-07 20:50:28 +0200

    Aggiunti commenti a metodi e attributi alla classe UserController.java

--- src/main/java/it/uniba/app/user/UserController.java
+++ src/main/java/it/uniba/app/user/UserController.java
@@ -1,8 +1,18 @@
 package it.uniba.app.user;
 
+/**
+ * {@literal <<control>>}
+ * Wraps a user entity and controls communications with it.
+ */
 public class UserController {
+
+    /** The currently active user object reference. */
     private final Player user;
 
+    /**
+     * @param playerType the user type you want to instantiate.
+     *                   Must be {@code "Player"} or {@code "Wordsmith"}.
+     */
     public UserController(final String playerType) {
         if (playerType.equals("Player")) {
             this.user = new Player();
@@ -14,10 +24,16 @@ public class UserController {
         }
     }
 
+    /**
+     * @return {@code true} if the user is a Wordsmith, {@code false} if not.
+     */
     public boolean isWordsmith() {
         return user.isWordsmith();
     }
 
+    /**
+     * @return a String array of all possible commands the user can use.
+     */
     public String[] getHelpCommands() {
         return user.getHelpCommands();
     }
