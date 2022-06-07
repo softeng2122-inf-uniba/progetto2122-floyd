9300e1465fe1ac4895ee4d21fb75c7d43dde1eae
orsinico <nicoorsi.no@gmail.com>
2022-06-07 20:47:53 +0200

    Aggiunti commenti a metodi e attributi alla classe Player.java

--- src/main/java/it/uniba/app/user/Player.java
+++ src/main/java/it/uniba/app/user/Player.java
@@ -1,9 +1,12 @@
 package it.uniba.app.user;
 
 /**
- * < < entity > > Gestisce i diritti del giocatore
+ * {@literal <<entity>>}
+ * Manages player's privileges.
  */
 public class Player {
+
+    /** The list of available commands. */
     private static final String[] COMMANDS = {
             "/help",
             "/gioca",
@@ -11,12 +14,17 @@ public class Player {
             "/esci"
     };
 
+    /** Defines the user's privileges. */
     private boolean wordsmith;
 
+    /** Initializes Player object, without Wordsmith privileges. */
     public Player() {
         wordsmith = false;
     }
 
+    /**
+     * @return a String array of all possible commands Player can use.
+     */
     public String[] getHelpCommands() {
         String[] copy = new String[COMMANDS.length];
         for (int i = 0; i < copy.length; i++) {
@@ -25,6 +33,9 @@ public class Player {
         return copy;
     }
 
+    /**
+     * @return {@code true} if the player is Wordsmith, {@code false} if not.
+     */
     public boolean isWordsmith() {
         return wordsmith;
     }
