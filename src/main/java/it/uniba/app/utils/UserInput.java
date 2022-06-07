0022461806d1ea1320a4e9b1215441adad3840ec
Pierdamiano Zagaria <crysis3775@gmail.com>
2022-06-07 14:26:56 +0200

    Aggiunto controllo su UserInput.get() per input vuoti

--- src/main/java/it/uniba/app/utils/UserInput.java
+++ src/main/java/it/uniba/app/utils/UserInput.java
@@ -9,6 +9,10 @@ public class UserInput {
     private static Scanner in = new Scanner(System.in, "UTF-8");
 
     public static String get() {
-        return in.nextLine().toLowerCase();
+        String input = in.nextLine().toLowerCase();
+        while (input.isEmpty()) {
+            input = in.nextLine().toLowerCase();
+        }
+        return input;
     }
 }
