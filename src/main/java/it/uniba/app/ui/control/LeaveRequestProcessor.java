c7cbd93e3299b47083c824edd1bae10c1ca47b43
orsinico <nicoorsi.no@gmail.com>
2022-06-07 19:59:10 +0200

    Aggiunti commenti a metodi e attributi alla classe LeaveRequestProcessor.java

--- src/main/java/it/uniba/app/ui/control/LeaveRequestProcessor.java
+++ src/main/java/it/uniba/app/ui/control/LeaveRequestProcessor.java
@@ -4,9 +4,18 @@ import it.uniba.app.match.controller.MatchController;
 import it.uniba.app.ui.UserInterface;
 import it.uniba.app.utils.ExecutableTask;
 
+/**
+ * Tries to leave the match, if there's one in progress.
+ * Prints error otherwise.
+ */
 public class LeaveRequestProcessor implements ExecutableTask {
+    
+    /** Reference to the match controller. */
     private final MatchController matchController;
 
+    /**
+     * @param matchControllerObj the reference to the match controller.
+     */
     public LeaveRequestProcessor(MatchController matchControllerObj) {
         this.matchController = matchControllerObj;
     }
