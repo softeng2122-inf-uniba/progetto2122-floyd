a3053129d199788d985354b7241a4489761a0dbe
orsinico <nicoorsi.no@gmail.com>
2022-06-07 19:56:09 +0200

    Aggiunti commenti a metodi e attributi alla classe ExitRequestProcessor.java

--- src/main/java/it/uniba/app/ui/control/ExitRequestProcessor.java
+++ src/main/java/it/uniba/app/ui/control/ExitRequestProcessor.java
@@ -3,11 +3,10 @@ package it.uniba.app.ui.control;
 import it.uniba.app.ui.UserInterface;
 import it.uniba.app.utils.ExecutableTask;
 
-/**
- * ExitRequestProcessor
- */
+/** Exits the application, after a confirmation prompt. */
 public class ExitRequestProcessor implements ExecutableTask {
 
+    /** Instantiates a ExitRequest task. */
     public ExitRequestProcessor() {
     }
 
