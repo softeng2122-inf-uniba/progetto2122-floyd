2d776664c6f6fbe9957c543b90c05a2f4d1d5551
Pierdamiano Zagaria <crysis3775@gmail.com>
2022-06-07 13:25:40 +0200

    Creata classe GuessAttemptManager

---
+++ src/main/java/it/uniba/app/match/controller/GuessAttemptManager.java
@@ -0,0 +1,23 @@
+package it.uniba.app.match.controller;
+
+import it.uniba.app.match.Guess;
+import it.uniba.app.match.Match;
+import it.uniba.app.utils.ExecutableTaskGeneric;
+
+public class GuessAttemptManager implements ExecutableTaskGeneric<Boolean> {
+
+    private final Match match;
+
+    public GuessAttemptManager(Match matchObj) {
+        this.match = matchObj;
+    }
+
+    @Override
+    public Boolean execute(String userInput) {
+        GuessController guessCopy = match.getGuess(match.getCurrentGuessCtr());
+        guessCopy.examineGuessAttempt(userInput, match.getSecretWord());
+        match.setGuess(guessCopy, match.getCurrentGuessCtr());
+        return guessCopy.isCorrect();
+    }
+
+}
