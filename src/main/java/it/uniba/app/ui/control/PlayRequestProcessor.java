4a7ee25a6acb36513b8925bc907c92245b069667
orsinico <nicoorsi.no@gmail.com>
2022-06-07 20:02:53 +0200

    Aggiunti commenti a metodi e attributi alla classe PlayRequestProcessor.java

--- src/main/java/it/uniba/app/ui/control/PlayRequestProcessor.java
+++ src/main/java/it/uniba/app/ui/control/PlayRequestProcessor.java
@@ -5,14 +5,26 @@ import it.uniba.app.ui.UserInterface;
 import it.uniba.app.utils.ConsoleUtils;
 import it.uniba.app.utils.ExecutableTaskString;
 
+/**
+ * Tries to start a match, if not already started.
+ * Prints error otherwise.
+ */
 public class PlayRequestProcessor implements ExecutableTaskString {
 
+    /** Reference to the match controller. */
     private MatchController matchController;
 
+    /**
+     * @param matchControllerObj the reference to the match controller.
+     */
     public PlayRequestProcessor(MatchController matchControllerObj) {
         this.matchController = matchControllerObj;
     }
 
+    /**
+     * @param lastSecretWord the latest secret word.
+     *                       Can be {@code null}.
+     */
     @Override
     public void execute(String lastSecretWord) {
         if (matchController.isInProgress()) {
