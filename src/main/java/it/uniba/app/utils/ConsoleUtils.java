9dc629f1176bd102ba3ac2feeacc500b2315934e
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:10:03 +0200

    Aggiunti commenti a metodi e attributi alla classe ConsoleUtils.java

--- src/main/java/it/uniba/app/utils/ConsoleUtils.java
+++ src/main/java/it/uniba/app/utils/ConsoleUtils.java
@@ -1,16 +1,27 @@
 package it.uniba.app.utils;
 
 /**
- * < < noECB > > Codici ANSI per visualizzazione avanzata in console
+ * {@literal <<noECB>>}
+ * Contains ANSI escape sequences for colors and clearing console.
  */
 public class ConsoleUtils {
+
+    /** Resets text color and background color. */
     public static final String ANSI_RESET = "\u001B[0m";
+
+    /** Sets text color to black. */
     public static final String ANSI_BLACK = "\u001B[30m";
 
+    /** Sets background color to green. */
     public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+
+    /** Sets background color to yellow. */
     public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+
+    /** Sets background color to white. */
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    /** Clears console. */
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
