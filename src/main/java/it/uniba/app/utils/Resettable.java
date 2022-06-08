43dbb03c2b97750120dd2359f4867fd4a96e9421
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:36:01 +0200

    Aggiunti commenti a metodi e attributi alla classe Resettable.java

--- src/main/java/it/uniba/app/utils/Resettable.java
+++ src/main/java/it/uniba/app/utils/Resettable.java
@@ -1,5 +1,12 @@
 package it.uniba.app.utils;
 
+/**
+ * {@literal <<noECB>>}
+ * A class that implements the Resettable interface can
+ * reset its state to default.
+ */
 public interface Resettable {
+    
+    /** Sets the object to default state. */
     void reset();
 }
