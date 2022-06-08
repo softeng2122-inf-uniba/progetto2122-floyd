f8607f38605a0a45b28b532ca2f277375b514cc1
Pierdamiano Zagaria <crysis3775@gmail.com>
2022-06-08 17:25:32 +0200

    Aggiunte test unit per Wordsmith.java

---
+++ src/test/java/it/uniba/app/user/WordsmithTest.java
@@ -0,0 +1,26 @@
+package it.uniba.app.user;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WordsmithTest {
+    private Wordsmith wordsmith;
+
+    @BeforeEach
+    public void setUp() {
+        wordsmith = new Wordsmith();
+    }
+
+    @Test
+    public void testGetHelpCommands() {
+        assertNotNull(wordsmith.getHelpCommands());
+    }
+
+    @Test
+    public void testIsWordsmith() {
+        assertTrue(wordsmith.isWordsmith());
+    }
+}
