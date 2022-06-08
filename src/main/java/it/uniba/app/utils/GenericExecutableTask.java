c23412fdece79f73a6ff8716c3f43b783e2ce5f4
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:25:46 +0200

    Aggiunti commenti a metodi e attributi alla classe GenericExecutableTask.java

--- src/main/java/it/uniba/app/utils/GenericExecutableTask.java
+++ src/main/java/it/uniba/app/utils/GenericExecutableTask.java
@@ -1,5 +1,10 @@
 package it.uniba.app.utils;
 
 public interface GenericExecutableTask<T> {
+    /**
+     * Executes a command on the object.
+     *
+     * @return something as result.
+     */
     T execute();
 }
