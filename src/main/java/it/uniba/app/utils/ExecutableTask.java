f67d0c739f4255200c911173b27e3a2f32407325
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:17:31 +0200

    Aggiunti commenti a metodi e attributi alla classe ExecutableTask.java

--- src/main/java/it/uniba/app/utils/ExecutableTask.java
+++ src/main/java/it/uniba/app/utils/ExecutableTask.java
@@ -1,5 +1,13 @@
 package it.uniba.app.utils;
-
+/**
+ * {@literal <<noECB>>}
+ * A class that implements this interface can
+ * execute a task without returning and passing anything.
+ * It indicates that there's only one task and
+ * nothing else.
+ */
 public interface ExecutableTask {
+
+    /** Executes a command on the object. */
     void execute();
 }
