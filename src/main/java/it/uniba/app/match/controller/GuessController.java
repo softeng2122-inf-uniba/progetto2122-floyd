5006af94a0ba4738427d82a208d44a9907c4f3eb
orsinico <nicoorsi.no@gmail.com>
2022-06-08 09:26:59 +0200

    Aggiunti commenti a metodi e attributi alla classe GuessController.java

--- src/main/java/it/uniba/app/match/controller/GuessController.java
+++ src/main/java/it/uniba/app/match/controller/GuessController.java
@@ -5,42 +5,77 @@ import it.uniba.app.match.Word;
 
 /**
  * {@literal <<control>>}
- * Controls a Guess object. Provides methods to check guess attempts.
+ * Manages a Guess object and its internal state.
+ * <p>
+ * Provides methods to check guess attempts.
  */
 public class GuessController {
+    
+     /** Reference to the guess object that this controller is responsible of. */
     private final Guess guess;
+
+    /** Reference to the secret word object this guess refers to. */
     private final Word secretWordToGuess;
 
+    /** Instantiates a guess controller. */
     public GuessController() {
         this.guess = new Guess();
         this.secretWordToGuess = new Word();
     }
 
+     /**
+     * Copy constructor. Instantiates a copied guess controller.
+     *
+     * @param gc the guess controller to copy.
+     */
     public GuessController(final GuessController gc) {
         this.guess = new Guess(gc.guess);
         this.secretWordToGuess = new Word(gc.secretWordToGuess);
     }
 
+     /**
+     * Starts the examination process on the given input.
+     *
+     * @param userInput the string to check.
+     */
     public void startExamination(final String userInput) {
         new GuessExaminer(guess, secretWordToGuess).execute(userInput);
     }
 
+    /**
+     * @param str the secret word to guess.
+     */
     public void setSecretWordToGuess(final String str) {
         secretWordToGuess.setString(str);
     }
 
+    /**
+     * @return {@code true} if the guess is correct, {@code false} otherwise.
+     */
     public boolean isCorrect() {
         return guess.isCorrect();
     }
 
+    /**
+     * @param idx index of the requested cell.
+     * @return a string representing ANSI sequence of the cell color.
+     */
     public String getCellColor(final int idx) {
         return guess.getCellColor(idx);
     }
 
+    /**
+     * @param idx index of the cell
+     * @return a char representing the cell's character.
+     */
     public char getCellCharacter(final int idx) {
         return guess.getCellCharacter(idx);
     }
 
+    /**
+     * Resets the guess and its cells
+     * to default state.
+     */
     public void reset() {
         guess.reset();
     }
