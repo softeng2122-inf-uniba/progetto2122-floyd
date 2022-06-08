db948c662a4ddf60db167c28d9833e8609dc31c8
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:29:11 +0200

    Aggiunti commenti a metodi e attributi alla classe GenericExecutableTaskString.java

--- src/main/java/it/uniba/app/utils/GenericExecutableTaskString.java
+++ src/main/java/it/uniba/app/utils/GenericExecutableTaskString.java
@@ -1,5 +1,23 @@
 package it.uniba.app.utils;
 
+/**
+ * {@literal <<noECB>>}
+ * A class that implements this interface can
+ * execute a task that returns something you choose.
+ * This task takes a string in input too.
+ * It indicates that there's only one task and
+ * nothing else.
+ *
+ * @param <T> the type you want to return
+ *            from the task.
+ */
 public interface GenericExecutableTaskString<T> {
+
+    /**
+     * Executes a command on the object.
+     *
+     * @param str the string to pass.
+     * @return something as result.
+     */
     T execute(String str);
 }
