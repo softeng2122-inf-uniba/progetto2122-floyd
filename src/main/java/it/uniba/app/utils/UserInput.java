209ad65145fb261bccfc22146d4fe7452165fc85
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:38:16 +0200

    Aggiunti commenti a metodi e attributi alla classe UserInput.java

--- src/main/java/it/uniba/app/utils/UserInput.java
+++ src/main/java/it/uniba/app/utils/UserInput.java
@@ -3,11 +3,20 @@ package it.uniba.app.utils;
 import java.util.Scanner;
 
 /**
- * < < noECB > > Gestisce gli input da tastiera
+ * {@literal <<noECB>>}
+ * Manages the input operations.
  */
 public class UserInput {
+
+    /** Scanner where we store the input stream. */
     private static Scanner in = new Scanner(System.in, "UTF-8");
 
+    /**
+     * Gets a line of input from the user.
+     *
+     * @return the String taken from user input,
+     *         automatically converted to lower case.
+     */
     public static String get() {
         String input = in.nextLine().toLowerCase();
         while (input.isEmpty()) {
