485e1e162c9b7c1e52e87bb1ca0428a03309e34d
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:34:21 +0200

    Aggiunti commenti a metodi e attributi alla classe InputChecker.java

--- src/main/java/it/uniba/app/utils/InputChecker.java
+++ src/main/java/it/uniba/app/utils/InputChecker.java
@@ -5,15 +5,38 @@ import java.util.regex.Pattern;
 
 import it.uniba.app.match.Match;
 
+/**
+ * {@literal <<noECB>>}
+ * Contains methods to check the input strings based
+ * on the game rules.
+ */
 public class InputChecker {
     private InputChecker() {
     }
-
+    
+    /**
+     * Checks if the input is a command.
+     * Only accepts strings that contain a "/" as first character.
+     *
+     * @param input The string to check.
+     * @return {@code true} if the word is a command, {@code false}
+     *         otherwise.
+     */
     public static boolean isCommand(String input) {
         Matcher matcher = Pattern.compile("/.+").matcher(input);
         return matcher.matches();
     }
 
+    /**
+     * Checks if the input is a valid Word.
+     * Only accepts strings that contain alphabet characters
+     * and that are long a specific number, given by
+     * {@link it.uniba.app.match.Match#NUM_OF_CELLS}.
+     *
+     * @param input The string to check.
+     * @return {@code true} if the word is valid, {@code false}
+     *         otherwise.
+     */
     public static boolean isValidAsWord(String input) {
         if (input.length() == Match.NUM_OF_CELLS) {
             Matcher matcher = Pattern.compile("[a-z]*").matcher(input);
