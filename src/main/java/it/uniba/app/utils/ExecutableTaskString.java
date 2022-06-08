af6b4d99624253bdae5c7d01cb23463153c9a737
orsinico <nicoorsi.no@gmail.com>
2022-06-08 13:23:29 +0200

    Aggiunti commenti a metodi e attributi alla classe ExecutableTaskString.java

--- src/main/java/it/uniba/app/utils/ExecutableTaskString.java
+++ src/main/java/it/uniba/app/utils/ExecutableTaskString.java
@@ -1,5 +1,18 @@
 package it.uniba.app.utils;
 
+/**
+ * {@literal <<noECB>>}
+ * A class that implements this interface can
+ * execute a task without returning anything but passing a string.
+ * It indicates that there's only one task and
+ * nothing else.
+ */
 public interface ExecutableTaskString {
+
+    /**
+     * Executes a command on the object.
+     *
+     * @param str the string to pass.
+     */
     void execute(String str);
 }
