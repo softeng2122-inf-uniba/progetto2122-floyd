e8614b3a70f83be8c41d066125a48bdf960e49ab
Pierdamiano Zagaria <crysis3775@gmail.com>
2022-06-09 13:43:04 +0200

    Aggiunte unità di test per Match.java

---
+++ src/test/java/it/uniba/app/match/MatchTest.java
@@ -0,0 +1,35 @@
+package it.uniba.app.match;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniba.app.match.controller.GuessController;
+import it.uniba.app.ui.UserInterface;
+import it.uniba.app.user.UserController;
+
+public class MatchTest {
+    private UserInterface ui;
+    private Match match;
+
+    @BeforeEach
+    public void setUp() {
+        ui = new UserInterface(new UserController("Wordsmith"));
+        match = new Match(ui);
+    }
+
+    @Test
+    public void testGetGuessControllers_IsDeepCopy() {
+        GuessController[] guessControllers = match.getGuessControllers();
+        assertNotSame(guessControllers, match.getGuessControllers());
+    }
+
+    @Test
+    public void testSetGuessControllers_IsDeepCopy() {
+        GuessController[] guessControllers = match.getGuessControllers();
+        guessControllers[0].setSecretWordToGuess("prova");
+        match.setGuessControllers(guessControllers);
+        assertNotSame(guessControllers, match.getGuessControllers());
+    }
+}
