1d9ea6b1648e874ca5dcbc72521bfd4ec826b03c
Nicolò <n.pino@studenti.uniba.it>
2022-06-09 17:48:31 +0200

    Aggiunti i commenti nella classe GuessAttemptManagerTest

--- src/test/java/it/uniba/app/match/controller/GuessAttemptManagerTest.java
+++ src/test/java/it/uniba/app/match/controller/GuessAttemptManagerTest.java
@@ -10,10 +10,17 @@ import it.uniba.app.match.Match;
 import it.uniba.app.ui.UserInterface;
 import it.uniba.app.user.UserController;
 
+/** Test GuessAttemptManager class. */
+
 public final class GuessAttemptManagerTest {
+
+    /** Match object. */
     private Match match;
+
+    /** UserInterface object. */
     private UserInterface ui;
 
+    /** Setups the test. */
     @BeforeEach
     public void setUp() {
         ui = new UserInterface(new UserController("Wordsmith"));
@@ -21,12 +28,20 @@ public final class GuessAttemptManagerTest {
         match.setSecretWord("prova");
     }
 
+    /**
+     * Test the execute method with a correct guess in input.
+     * Checks if it returns true, since the guess is correct.
+     */
     @Test
     public void testExecute_CorrectGuess() {
         assertTrue(
                 new GuessAttemptManager(match).execute("prova"));
     }
 
+    /**
+     * Test the execute method with an incorrect guess in input.
+     * Checks if it returns false, since the guess is incorrect.
+     */
     @Test
     public void testExecute_IncorrectGuess() {
         assertFalse(
