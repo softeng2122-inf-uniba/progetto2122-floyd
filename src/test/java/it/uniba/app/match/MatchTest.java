c3c8fee1b79673b02cdaab45462ac262f0c01cef
Nicolò <n.pino@studenti.uniba.it>
2022-06-09 18:29:46 +0200

    Aggiunti i commenti nella classe MatchTest

--- src/test/java/it/uniba/app/match/MatchTest.java
+++ src/test/java/it/uniba/app/match/MatchTest.java
@@ -9,22 +9,28 @@ import it.uniba.app.match.controller.GuessController;
 import it.uniba.app.ui.UserInterface;
 import it.uniba.app.user.UserController;
 
+/** Test Match class. */
 public final class MatchTest {
+    /** UserInterface object. */
     private UserInterface ui;
+    /** Class to test. */
     private Match match;
 
+    /** Setups the test. */
     @BeforeEach
     public void setUp() {
         ui = new UserInterface(new UserController("Wordsmith"));
         match = new Match(ui);
     }
 
+    /** Test getGuessControllers to verify if it's an actual deep copy. */
     @Test
     public void testGetGuessControllers_IsDeepCopy() {
         GuessController[] guessControllers = match.getGuessControllers();
         assertNotSame(guessControllers, match.getGuessControllers());
     }
 
+    /** Test setGuessControllers to verify if it saves a new deep copy. */
     @Test
     public void testSetGuessControllers_IsDeepCopy() {
         GuessController[] guessControllers = match.getGuessControllers();
