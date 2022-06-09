a194330bc745c00e2541b843cd1cf6ba63bfec44
Nicolò <n.pino@studenti.uniba.it>
2022-06-09 19:05:49 +0200

    Aggiunti i commenti nella classe UserControllerTest

--- src/test/java/it/uniba/app/user/UserControllerTest.java
+++ src/test/java/it/uniba/app/user/UserControllerTest.java
@@ -7,37 +7,47 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/** Tests UserController class. */
 public final class UserControllerTest {
+    /** Class to test for player. */
     private UserController playerController;
+
+    /** Class to test for wordsmith. */
     private UserController wordsmithController;
 
+    /** Setups the test. */
     @BeforeEach
     public void setUp() {
         playerController = new UserController("Player");
         wordsmithController = new UserController("Wordsmith");
     }
 
+    /** Tests the instantiation with an illegal player type. */
     @Test
     public void testUserController_invalidPlayerType() {
         assertThrowsExactly(IllegalArgumentException.class,
                 () -> new UserController("illegal"));
     }
 
+    /** Test isWordsmith method for player. */
     @Test
     public void testIsWordsmith_Player() {
         assertFalse(playerController.isWordsmith());
     }
 
+    /** Test isWordsmith method for wordsmith. */
     @Test
     public void testIsWordsmith_Wordsmith() {
         assertTrue(wordsmithController.isWordsmith());
     }
 
+    /** Test getHelpCommands for player. */
     @Test
     public void testGetHelpCommands_Player_NotEmpty() {
         assertTrue(playerController.getHelpCommands().length > 0);
     }
 
+    /** Test getHelpCommands for wordsmith. */
     @Test
     public void testGetHelpCommands_Wordsmith_NotEmpty() {
         assertTrue(wordsmithController.getHelpCommands().length > 0);
